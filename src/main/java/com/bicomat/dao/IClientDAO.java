3dc0e23430eca918436e54282b291edcb3397bf5
Julien <sutre.julien@gmail.com>
2018-11-20 15:07:12 +0100

    Create IClientDAO.java

---
+++ src/main/java/com/bicomat/dao/IClientDAO.java
@@ -0,0 +1,34 @@
+package com.bicomat.dao;
+
+import java.util.List;
+
+import com.bicomat.bean.Client;
+
+public interface IClientDAO {
+
+	/**
+	 * Ajoute une client.
+	 *
+	 * @param b Client à ajouter
+	 */
+	public void ajouterClient(Client c);
+	/**
+	 * Modifie une client.
+	 *
+	 * @param b Client à modifier
+	 */
+	public void modifierClient(Client c);
+	/**
+	 * Supprime une client.
+	 *
+	 * @param id Id de la client à supprimer
+	 */
+	public void supprimerClient(int id);
+	
+	/**
+	 * Retourne la liste des clients.
+	 *
+	 * @return la liste des clients de la table
+	 */
+	public List<Client> listeClients();
+}
