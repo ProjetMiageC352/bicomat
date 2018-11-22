d7e8a7f18f0e5992c2422e4d74660adbe570ba4c
Julien <sutre.julien@gmail.com>
2018-11-22 09:02:32 +0100

    Update TiersController.java

--- src/main/java/com/bicomat/controller/TiersController.java
+++ src/main/java/com/bicomat/controller/TiersController.java
@@ -2,9 +2,12 @@ package com.bicomat.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,7 +16,7 @@ import com.bicomat.bean.Tiers;
 import com.bicomat.service.ITiersService;
 
 @Controller
-@RequestMapping(value="/tiers")
+
 public class TiersController {
 	
 	@Autowired
@@ -25,7 +28,7 @@ public class TiersController {
 		this.tiersService = ts;
 	}
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value="/tiers",method = RequestMethod.GET)
 	public String listPersons(ModelMap pModel) {
 		final List<Tiers> lTierss = tiersService.listeTiers();
         pModel.addAttribute("Tiers", lTierss);
@@ -33,7 +36,27 @@ public class TiersController {
         return "tiers"; //jsp de redirection 
 	}
 	
-
+	@RequestMapping(value="/creertiers",method = RequestMethod.GET)
+	public String creerClient(Model Model) {
+		
+        Model.addAttribute("creertiers", new Tiers());
+       
+        return "formtiers"; //jsp de redirection 
+	}
 	
+	@RequestMapping(value="/creertiers",method = RequestMethod.POST)
+	public String addtiers(HttpServletRequest response) {
+		Tiers tiers =new Tiers();
+		tiers.setNumCompte(Integer.parseInt(response.getParameter("num")));
+	    if (response.getParameter("actif")=="true"){
+	    	tiers.setActif(true);
+	    }else {
+	    	tiers.setActif(false);
+	    }
+	    tiersService.creerTiers(tiers);
+		
+       
+        return "formtiers"; //jsp de redirection 
+	}
 	
 }
