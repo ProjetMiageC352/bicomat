6b022a93b72a1f6a82ac08a79794d489c989c192
Julien <sutre.julien@gmail.com>
2018-11-22 10:33:44 +0100

    Update ClientController.java

--- src/main/java/com/bicomat/controller/ClientController.java
+++ src/main/java/com/bicomat/controller/ClientController.java
@@ -2,9 +2,12 @@ package com.bicomat.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,7 +16,7 @@ import com.bicomat.bean.Client;
 import com.bicomat.service.IClientService;
 
 @Controller
-@RequestMapping(value="/clients")
+@RequestMapping()
 public class ClientController {
 	
 	@Autowired
@@ -25,7 +28,7 @@ public class ClientController {
 		this.clientService = cs;
 	}
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value="/clients",method = RequestMethod.GET)
 	public String listPersons(ModelMap pModel) {
 		final List<Client> lClients = clientService.listeClients();
         pModel.addAttribute("clients", lClients);
@@ -33,16 +36,28 @@ public class ClientController {
         return "client"; //jsp de redirection 
 	}
 	
-	/*@RequestMapping( method = RequestMethod.POST)
-    public String submit( ModelMap model) {
-      Client client = new Client();
-        model.addAttribute("nom", client.getNom());
-        model.addAttribute("prenom", client.getPrenom());
-        model.addAttribute("adresse", client.getAdresse_mail());
-        model.addAttribute("type", client.getType_client());
-        clientService.creerClient(client);
-        return "formclient";
-    }*/
+	@RequestMapping(value="/creerclients",method = RequestMethod.GET)
+	public String creerClient(Model Model) {
+		
+        Model.addAttribute("creerclient", new Client());
+       
+        return "formclient"; //jsp de redirection 
+	}
 	
+	@RequestMapping(value="/creerclients", method = RequestMethod.POST)
+    public String addclient( HttpServletRequest response) {
+     Client client= new Client();
+     client.setNom(response.getParameter("nom"));
+     client.setPrenom(response.getParameter("prenom"));
+     client.setAdresse_mail(response.getParameter("adresse"));
+     client.setTel_client(Integer.parseInt(response.getParameter("tel")));
+     client.setNum_contrat(Integer.parseInt(response.getParameter("contrat")));
+     client.setId_conseiller(Integer.parseInt(response.getParameter("conseiller")));
+     client.setNum_contrat(Integer.parseInt(response.getParameter("contrat")));
+     client.setLogin(response.getParameter("login"));
+     client.setPassword(response.getParameter("password"));
+      clientService.creerClient(client);
+      return "formclient";
+    }
 	
 }
