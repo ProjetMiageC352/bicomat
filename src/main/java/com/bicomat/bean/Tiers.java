5feb9d53bcf3445ddfb705a2a667293fbdd3443e
Julien <sutre.julien@gmail.com>
2018-11-22 10:33:41 +0100

    Update Tiers.java

--- src/main/java/com/bicomat/bean/Tiers.java
+++ src/main/java/com/bicomat/bean/Tiers.java
@@ -18,32 +18,27 @@ public class Tiers  {
 	@Id
 	@Column(name="t_id")
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int idT;
-	
+	private int id;
 	/**
 	 * Nom du tiers
 	 */
 	@Column(name="t_nom")
-	private String nomT;
-	
+	private String nom;
 	/**
 	 * Prenom du tiers
 	 */
 	@Column(name="t_prenom")
-	private String prenomT;
-	
+	private String prenom;
 	/**
 	 * Numéro de compte du tiers
 	 */
 	@Column(name="t_num_compte")
 	private int numCompte;
-	
 	/**
 	 * Id du client associé
 	 */
 	@Column(name="t_id_client")
 	private int idclientT;
-	
 	/**
 	 * Client actif?
 	 */
@@ -65,14 +60,19 @@ public class Tiers  {
     /**
      * Constructeur de la classe.
      */
-	 /**
-     * @param id_T
+	
+    /**
+     * @param nomT
+     * @param prenomT
      * @param numCompteT
+     * @param id_clientT
      * @param actifT
      */
-    public Tiers(final int numCompteT, final boolean actifT) {
-    	
+    public Tiers(String nomT,String prenomT,int numCompteT, int id_clientT,boolean actifT) {
+    	this.nom=nomT;
+    	this.prenom=prenomT;
     	this.numCompte=numCompteT;
+    	this.idclientT=id_clientT;
     	this.actif=actifT;
     }
     
@@ -85,15 +85,32 @@ public class Tiers  {
     /**
 	 * @return the id
 	 */
-	public final int getIdT() {
-		return idT;
+	public final int getId() {
+		return id;
 	}
-	
+	/**
+	 * @return the nomT
+	 */
+	public final String getNom() {
+		return nom;
+	}
+	/**
+	 * @return the prenomT
+	 */
+	public final String getPrenom() {
+		return prenom;
+	}
     /**
 	 * @return the numCompte
 	 */
 	public final int getNumCompte() {
 		return numCompte;
+	}
+	/**
+	 * @return the idclientT
+	 */
+	public final int getIdclientT() {
+		return idclientT;
 	}
 	/**
 	 * @return the actif
@@ -108,14 +125,33 @@ public class Tiers  {
 	/**
 	 * @param id the id to set
 	 */
-	public final void setIdT(int id) {
-		this.idT = id;
+	public final void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @param nomT the nomT to set
+	 */
+	public final void setNom(String nomT) {
+		this.nom = nomT;
+	}
+
+	/**
+	 * @param prenomT the prenomT to set
+	 */
+	public final void setPrenom(String prenomT) {
+		this.prenom = prenomT;
 	}
 	/**
 	 * @param numCompte the numCompte to set
 	 */
 	public final void setNumCompte(int numCompte) {
 		this.numCompte = numCompte;
+	}
+	/**
+	 * @param idclientT the idclientT to set
+	 */
+	public final void setIdclientT(int idclientT) {
+		this.idclientT = idclientT;
 	}
 	/**
 	 * @param actif the actif to set
@@ -129,8 +165,7 @@ public class Tiers  {
 	 */
 	@Override
 	public String toString() {
-		return "Tiers [idT=" + idT + ", numCompte=" + numCompte + ", actif=" + actif + "]";
-	}
-	
-   
+		return "Tiers [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", numCompte=" + numCompte
+				+ ", idclientT=" + idclientT + ", actif=" + actif + "]";
+	}   
 }
