7c7ad789c70e9edfdc6fd87d9184ce2ce8210da9
yildumit <44347509+yildumit@users.noreply.github.com>
2018-11-22 18:53:03 +0100

    Create interface.java

---
+++ src/main/java/com/bicomat/bean/interface.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+/**
+ * 
+ */
+public class Interface {
+
+    /**
+     * Default constructor
+     */
+    public Interface() {
+    }
+
+    /**
+     * 
+     */
+    public void Attendre24h() { //le client devra attendre 24h pour l'activation de son compte
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void FournirRenseignement() { //saisie du nom, prénom...
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void DemanderRelevéOpération() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void AfficherListe() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void CréerCompte() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void CréerCompteAgency() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void ClôturerCompte() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void CalculerIntérêts() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void CalculerImpôts() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Créer_virement() {
+        // TODO implement here
+    }
+
+}
