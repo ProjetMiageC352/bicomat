658ecfc0ae493707094250d9bca9b726019d4728
Julien <sutre.julien@gmail.com>
2018-11-23 22:06:57 +0100

    Update ClientDAO.java

--- src/main/java/com/bicomat/dao/ClientDAO.java
+++ src/main/java/com/bicomat/dao/ClientDAO.java
@@ -12,6 +12,7 @@ import javax.persistence.criteria.Root;
 import org.springframework.stereotype.Repository;
 
 import com.bicomat.bean.Client;
+import com.bicomat.bean.CarteBancaire;
 
 @Repository
 public class ClientDAO implements IClientDAO {
@@ -27,6 +28,72 @@ public class ClientDAO implements IClientDAO {
 	public void creerClient(Client c) {
 		entityManager.persist(c);
 	}
+	/**
+	 * Demande de vérifier si un Client existe pour un nom, prenom et numero de CB
+	 * @param nom
+	 * @param prenom
+	 * @return
+	 */
+	public boolean existeAvecNomPrenomNumcarte(String nom, String prenom,String numcarte ) {
+		boolean existe = false;
+		final CriteriaBuilder lCriteriaBuilder = entityManager.getCriteriaBuilder();
+		final CriteriaQuery<Client> lCriteriaQuery = lCriteriaBuilder.createQuery(Client.class);
+		final Root<Client> lRoot = lCriteriaQuery.from(Client.class);
+		lCriteriaQuery.multiselect(lRoot.get("id"));
+		
+		lCriteriaQuery.where(lCriteriaBuilder.and(
+        		lCriteriaBuilder.equal(lRoot.get("nom"), nom),
+        		lCriteriaBuilder.equal(lRoot.get("prenom"), prenom)        		
+        		));
+		TypedQuery<Client> query = entityManager.createQuery(lCriteriaQuery);
+		List<Client> ListeClient = query.getResultList();
+	
+		if (!ListeClient.isEmpty()) {
+			int Id_client = query.getSingleResult().getId();
+			final CriteriaBuilder lCriteriaBuilder2 = entityManager.getCriteriaBuilder();
+			final CriteriaQuery<CarteBancaire> lCriteriaQuery2 = lCriteriaBuilder2.createQuery(CarteBancaire.class);
+			final Root<CarteBancaire> lRoot2 = lCriteriaQuery2.from(CarteBancaire.class);
+			lCriteriaQuery2.select(lRoot2);
+			lCriteriaQuery2.where(lCriteriaBuilder2.and(
+	        		lCriteriaBuilder2.equal(lRoot2.get("idC"), Id_client)       		
+	        		));
+			TypedQuery<CarteBancaire> query2 = entityManager.createQuery(lCriteriaQuery2);
+	    	List<CarteBancaire> ListeCarteBancaire = query2.getResultList();
+	    	if (!ListeCarteBancaire.isEmpty()) {
+	    	existe = true;
+	    	}
+	    	 return existe;
+	    }
+	    return existe;
+	}
+	
+	/**
+	 * Demande de vérifier si le client a un compte agency
+	 * @param nom
+	 * @param prenom
+	 * @return
+	 */
+	public boolean verifieclientAgency(String nom, String prenom) {
+		boolean compteagency = false;
+		final CriteriaBuilder lCriteriaBuilder = entityManager.getCriteriaBuilder();
+		final CriteriaQuery<Client> lCriteriaQuery = lCriteriaBuilder.createQuery(Client.class);
+		final Root<Client> lRoot = lCriteriaQuery.from(Client.class);
+		lCriteriaQuery.multiselect(lRoot.get("login"));
+		lCriteriaQuery.where(lCriteriaBuilder.and(
+        		lCriteriaBuilder.equal(lRoot.get("nom"), nom),
+        		lCriteriaBuilder.equal(lRoot.get("prenom"), prenom)
+        		
+        		));
+		TypedQuery<Client> query = entityManager.createQuery(lCriteriaQuery);
+		String LoginClient = query.getSingleResult().getLogin();
+	
+		if (LoginClient !=null) {
+			compteagency = true;
+	    }
+	    return compteagency;
+	}
+	
+	
 	/**
 	 * Modifie une client.
 	 *
