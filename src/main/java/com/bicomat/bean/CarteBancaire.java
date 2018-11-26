fd72cd7f005b018b2ef19fa15801be6915754e54
a-pitet <adeline.pitet@get-mail.fr>
2018-11-26 08:54:46 +0100

    Correction de la classe de test CarteBancaire

--- src/main/java/com/bicomat/bean/CarteBancaire.java
+++ src/main/java/com/bicomat/bean/CarteBancaire.java
@@ -1,6 +1,6 @@
 package com.bicomat.bean;
 
-import java.util.Date;
+import java.sql.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
--- src/test/java/com/bicomat/bean/CarteBancaireTest.java
+++ src/test/java/com/bicomat/bean/CarteBancaireTest.java
@@ -1,5 +1,7 @@
 package com.bicomat.bean;
 
+import java.sql.Date;
+
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -19,7 +21,7 @@ public class CarteBancaireTest {
      */
     @BeforeClass
     public static void setUpBeforeClass() {
-        cb = new CarteBancaire(1, "0001", "Type", 1, "001", 1);
+        cb = new CarteBancaire(1, "0001", "Type", Date.valueOf("2018-01-01"), 1);
     }
     
     /* **************************** */
@@ -32,8 +34,8 @@ public class CarteBancaireTest {
     @Test
     public final void testConstructeur() {
         Assert.assertNotNull(new CarteBancaire());
-        Assert.assertNotNull(new CarteBancaire("0001", "Type", 1, "001", 1));
-        Assert.assertNotNull(new CarteBancaire(1, "0001", "Type", 1, "001", 1));
+        Assert.assertNotNull(new CarteBancaire("0001", "Type", Date.valueOf("2018-01-01"), 1));
+        Assert.assertNotNull(new CarteBancaire(1, "0001", "Type", Date.valueOf("2018-01-01"), 1));
     }
     
     /* **************************** */
@@ -60,13 +62,6 @@ public class CarteBancaireTest {
     @Test
     public final void testGetType_carte() {
         Assert.assertEquals(cb.getType_carte(), "Type");
-    }
-    /**
-     * Test de getCode_crypto().
-     */
-    @Test
-    public final void testGetCode_crypto() {
-        Assert.assertEquals(cb.getCode_crypto(), "001");
     }
     /**
      * Test de getCb_idclient().
@@ -115,20 +110,10 @@ public class CarteBancaireTest {
      */
     @Test
     public final void testSetEcheance() {
-        cb.setEcheance(2);
-        Assert.assertEquals(cb.getEcheance(), 2);
-        cb.setEcheance(1);
-        Assert.assertEquals(cb.getEcheance(), 1);
-    }
-    /**
-     * Test de setCode_crypto(String code_crypto).
-     */
-    @Test
-    public final void testSetCode_crypto() {
-        cb.setCode_crypto("002");
-        Assert.assertEquals(cb.getCode_crypto(), "002");
-        cb.setCode_crypto("001");
-        Assert.assertEquals(cb.getCode_crypto(), "001");
+        cb.setEcheance(Date.valueOf("2017-01-01"));
+        Assert.assertEquals(cb.getEcheance(), Date.valueOf("2017-01-01"));
+        cb.setEcheance(Date.valueOf("2018-01-01"));
+        Assert.assertEquals(cb.getEcheance(), Date.valueOf("2018-01-01"));
     }
     /**
      * Test de setCb_idclient(int cb_idclient).
@@ -150,7 +135,7 @@ public class CarteBancaireTest {
      */
     @Test
     public final void testToString() {
-        Assert.assertEquals(cb.toString(), "CarteBancaire [idC=1, num_carte=0001, "
-        		+ "type_carte=Type, echeance=1, code_crypto=001, cb_idclient=1]");
+    	Assert.assertEquals(cb.toString(), "CarteBancaire [idC=1, num_carte=0001, "
+        		+ "type_carte=Type, echeance=2018-01-01, cb_idclient=1]");
     }
 }
