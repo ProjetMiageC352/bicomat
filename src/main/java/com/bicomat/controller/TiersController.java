823386e1b605c171e84679650968fe1e185474b2
Julien <sutre.julien@gmail.com>
2018-11-28 12:17:51 +0100

    Update TiersController.java

--- src/main/java/com/bicomat/controller/TiersController.java
+++ src/main/java/com/bicomat/controller/TiersController.java
@@ -1,6 +1,8 @@
 package com.bicomat.controller;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -11,13 +13,14 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.bicomat.bean.Client;
 import com.bicomat.bean.Tiers;
+import com.bicomat.service.IClientService;
 import com.bicomat.service.ITiersService;
 
 @Controller
@@ -26,11 +29,17 @@ public class TiersController {
 	
 	@Autowired
 	private ITiersService tiersService;
+	private IClientService clientService;
 	
 	@Autowired(required=true)
 	@Qualifier(value="tiersService")
 	public void settiersService(ITiersService ts){
 		this.tiersService = ts;
+	}
+	@Autowired(required=true)
+	@Qualifier(value="clientService")
+	public void setclientService(IClientService cs){
+		this.clientService = cs;
 	}
 	
 	@RequestMapping(value="/tiers",method = RequestMethod.GET)
@@ -41,19 +50,23 @@ public class TiersController {
 				if (session.getAttribute("conseiller") == null) {
 					request.getRequestDispatcher("connexion").forward(request, response);
 				}
+				
+		// renvoie la liste des tiers		
 		final List<Tiers> lTierss = tiersService.listeTiers();
         pModel.addAttribute("Tiers", lTierss);
-       
+       //renvoie la liste des clients
+        final List<Client> lClients = clientService.listeClients();
+		pModel.addAttribute("clients", lClients);	
         return "tiers/tiers"; //jsp de redirection 
 	}
 	
-	@RequestMapping(value="/ajouttiers",method = RequestMethod.GET)
+	/*@RequestMapping(value="/ajouttiers",method = RequestMethod.GET)
 	public String creerClient(Model Model) {
 		
         Model.addAttribute("creertiers", new Tiers());
        
         return "tiers/formtiers"; //jsp de redirection 
-	}
+	}*/
 	
 	@RequestMapping(value="/ajouttiers",method = RequestMethod.POST)
 	public String addtiers(HttpServletRequest response) {
@@ -63,6 +76,8 @@ public class TiersController {
 		tiers.setNumCompte(response.getParameter("num"));
 		tiers.setIdclientT(Integer.parseInt(response.getParameter("id_client")));
 	   
+		
+		
 	    tiersService.creerTiers(tiers);
 		
        
@@ -75,10 +90,15 @@ public class TiersController {
 			HttpServletResponse response) throws ServletException, IOException {
 		
 		// Redirection si le conseiller n'est pas connectÃ©
-				/*HttpSession session = request.getSession();
+				HttpSession session = request.getSession();
 				if (session.getAttribute("conseiller") == null) {
 					request.getRequestDispatcher("connexion").forward(request, response);
-				}*/
+				}
+				
+		//récupération de la liste de client pour formulaire		
+		final List<Client> lClients = clientService.listeClients();
+		pModel.addAttribute("clients", lClients);	
+				
 		return "tiers/creerTiers";
 	}
 	
@@ -87,22 +107,24 @@ public class TiersController {
 			@RequestParam("nom") String nomT,
 			@RequestParam("prenom") String prenomT,
 			@RequestParam("num") String numT,
-			@RequestParam("id_client") String idClient,
-			@RequestParam("actif") boolean actifC
+			@RequestParam("id_client") String idClient
+			
 			){
 		
+		// on vérifie si le tiers n'est pas déjà rattaché au client
 		if (tiersService.existeAvecNomPrenomNumCompte(nomT, prenomT,numT,Integer.parseInt(idClient))) {
-			String etatTiers="Ce tiers est dï¿½jï¿½ rattachï¿½ ï¿½ votre compte";
+			String etatTiers="Ce tiers est déjà rattaché à votre compte";
 			pModel.addAttribute("etatTiers",etatTiers);
 			return "tiers/creerTiers";
 		}else {
 		
+		// sinon on créé un nouveau tiers	
 		Tiers tiers=new Tiers();
 		tiers.setNom(nomT);
 		tiers.setPrenom(prenomT);
 		tiers.setNumCompte(numT);
 		tiers.setIdclientT(Integer.parseInt(idClient));
-	    
+	    tiers.setDate_creation(Date.valueOf(LocalDate.now()));
 		tiersService.creerTiers(tiers);
 		String etatTiers="Le tiers a bien été créé";
 		pModel.addAttribute("etatTiers", etatTiers);
